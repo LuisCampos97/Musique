d0b035480bd7d374d446df47af13f4398d975d88
Luís Manuel Martins Campos <2192735@c2-lcm-m06.local>
2019-11-21 14:53:45 +0000

    Conclusao da implementacao da classe do botao

--- Musique/CustomButton.swift
+++ Musique/CustomButton.swift
@@ -1,24 +1,30 @@
 import UIKit
 
+@IBDesignable
 class CustomButton: UIButton {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupButton()
-    }
+    @IBInspectable var color: UIColor = UIColor.orange
     
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupButton()
-    }
-    
-    
-    func setupButton() {
+    override func layoutSubviews() {
         setTitleColor(.white, for: .normal)
-        backgroundColor = UIColor.orange
+        setBackgroundColor(color: UIColor.orange, forState: .highlighted)
+        backgroundColor = color
         layer.cornerRadius = 7
-        //frame.width = 170
-        //frame.height = 30
+        
+        if let label = titleLabel {
+            label.textAlignment = .center
+        }
+    }
+    
+    func setBackgroundColor(color: UIColor, forState: UIControl.State) {
+        self.clipsToBounds = true
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+            let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            self.setBackgroundImage(colorImage, for: forState)
+        }
     }
 }
