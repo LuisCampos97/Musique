3497818e28205c7b0b64c9848e36cd09833dcd51
Luis Campos <luismmc1997@hotmail.com>
2019-11-27 14:49:02 +0000

    Configuraçao do Firebase no projeto.
    Adicao de pods (libraries) e comeco da implementacao do Login

--- Musique/View Controllers/LoginViewController.swift
+++ Musique/View Controllers/LoginViewController.swift
@@ -1,11 +1,35 @@
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: CustomButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func loginTapped(_ sender: Any) {
+        loginButton.color = UIColor.black
+        
+        //Validate Text Fields
+        
+        let email = usernameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        //Signing in the user
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if (error != nil) {
+                //Couldn't Sign in
+                
+                //TODO: Show Error in label
+            }
+            else {
+                //Transition to HomeView
+            }
+        }
     }
 }
---
+++ Pods/Target Support Files/Pods-Musique-MusiqueUITests/Pods-Musique-MusiqueUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Musique_MusiqueUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Musique_MusiqueUITests
+@end
