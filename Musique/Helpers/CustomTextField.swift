880f51aaa8e20a1ba510ebff12f8e4c5c0da68e4
Luis Campos <luismmc1997@hotmail.com>
2019-11-27 13:15:27 +0000

    Criaçao de um custom textfield e alteracoes na interface

---
+++ Musique/Helpers/CustomTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTextField.swift
+//  Musique
+//
+//  Created by Luís Manuel Martins Campos on 21/11/2019.
+//  Copyright © 2019 Luís Manuel Martins Campos. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
