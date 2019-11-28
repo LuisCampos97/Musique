e7226b1d31855f114365a1aae6a96e941353a039
Rodrigo Henrique Frazão Santos <rodrigohfsantos@hotmail.com>
2019-11-28 16:21:55 +0000

    TabBar and Edit Interfaces

---
+++ Musique/View Controllers/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditViewController.swift
+//  Musique
+//
+//  Created by Rodrigo Henrique Frazão Santos on 28/11/2019.
+//  Copyright © 2019 Luís Manuel Martins Campos. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
