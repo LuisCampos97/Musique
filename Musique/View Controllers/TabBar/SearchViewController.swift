475d5bf9ea871eafb46650a143d0fedad5823a8f
Luis Campos <luismmc1997@hotmail.com>
2020-01-14 21:19:38 +0000

    Alteração do TabBar antigo para um TabBarController e respetivos itens

---
+++ Musique/View Controllers/TabBar/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  Musique
+//
+//  Created by Luís Manuel Martins Campos on 14/01/2020.
+//  Copyright © 2020 Luís Manuel Martins Campos. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
